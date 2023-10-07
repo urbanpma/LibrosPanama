863d55041ab51e92089e4e06758baf7f6f4ff1e7
UrbanPMA <urbanpma30@gmail.com>
2023-10-06 20:45:48 -0500

    Home Activity

--- app/src/main/java/com/ubnsoftware/librospanama/MainActivity.java
+++ app/src/main/java/com/ubnsoftware/librospanama/MainActivity.java
@@ -10,17 +10,28 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btnRegistrar;
+    Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnRegistrar = findViewById(R.id.btn_crearcuenta);
+        btnLogin = findViewById(R.id.btn_iniciar);
 
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
