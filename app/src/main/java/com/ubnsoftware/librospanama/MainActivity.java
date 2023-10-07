b3b448afd5f0cc1fc580d3af5fff7a8eb79aa7d6
UrbanPMA <urbanpma30@gmail.com>
2023-10-06 19:54:32 -0500

    Registro UI

--- app/src/main/java/com/ubnsoftware/librospanama/MainActivity.java
+++ app/src/main/java/com/ubnsoftware/librospanama/MainActivity.java
@@ -2,13 +2,28 @@ package com.ubnsoftware.librospanama;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegistrar = findViewById(R.id.btn_crearcuenta);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/ubnsoftware/librospanama/RegistroActivity.java
@@ -0,0 +1,16 @@
+package com.ubnsoftware.librospanama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+    }
+}
